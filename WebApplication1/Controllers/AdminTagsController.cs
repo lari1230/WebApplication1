f26b4c7d76ad96a4db82d7e467a22cf7ca73d566
Selkin <Selkin@DESKTOP-VE1J79R>
2024-09-21 20:18:24 +0300

    ...

--- WebApplication1/Controllers/AdminTagsController.cs
+++ WebApplication1/Controllers/AdminTagsController.cs
@@ -1,14 +1,34 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Data;
+using WebApplication1.Models.ViewModals;
 
 namespace WebApplication1.Controllers
 {
     public class AdminTagsController : Controller
     {
-        // GET: AdminTagsController
+        private readonly BloggieDbContext bloggieDbContext;
+
+        public AdminTagsController(BloggieDbContext bloggieDbContext) 
+        {
+            this.bloggieDbContext = bloggieDbContext;
+        }
+        [HttpGet]
         public IActionResult Add()
         {
             return View();
+        }
+        [HttpPost]
+        [ActionName("Add")]
+        public IActionResult Add(AddTagRequest addTagReauest) 
+        {
+            var tag = new Models.Domain.Tag
+            {
+                Name = addTagReauest.Name,
+                DisplayName = addTagReauest.DisplayName
+            };
+            bloggieDbContext.Tags.Add(tag);
+            return View("Add");
         }
 
         
